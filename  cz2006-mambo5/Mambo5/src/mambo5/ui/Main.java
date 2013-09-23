39497592c7de6f420ba53171b3aef11300ff47e4
Juliuschenn@gmail.com <Juliuschenn@gmail.com@861b9790-7418-99c2-d497-464402a33044>
2013-09-23 07:31:56 +0000

    Removed a comment

---  cz2006-mambo5/Mambo5/src/mambo5/ui/Main.java
+++  cz2006-mambo5/Mambo5/src/mambo5/ui/Main.java
@@ -23,9 +23,7 @@ public class Main extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
