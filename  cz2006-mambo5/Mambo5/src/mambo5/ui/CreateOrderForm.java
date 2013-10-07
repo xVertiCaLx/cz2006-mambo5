7821522ee75faa724a7d7d6572c5aef1ba96ce66
aloysius.ongrh@gmail.com <aloysius.ongrh@gmail.com@861b9790-7418-99c2-d497-464402a33044>
2013-10-07 13:48:18 +0000

    Changed CreateOrderForm into JPanel so that we can switch "page".

---  cz2006-mambo5/Mambo5/src/mambo5/ui/CreateOrderForm.java
+++  cz2006-mambo5/Mambo5/src/mambo5/ui/CreateOrderForm.java
@@ -19,34 +19,10 @@ import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class CreateOrderForm extends JFrame {
+public class CreateOrderForm extends JPanel {
 
-	private JPanel contentPane;
-	/**
-	 * @wbp.nonvisual location=291,19
-	 */
+	//private JPanel contentPane;
 	private final JLabel lblOrderId = new JLabel("NEW ORDER #xxx");
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					CreateOrderForm frame = new CreateOrderForm();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 * @return 
-	 */
 	final JTextArea receipt = new JTextArea();
 	
 	public String generateTemplate()
@@ -68,19 +44,13 @@ public class CreateOrderForm extends JFrame {
 	}
 	
 	public CreateOrderForm() {
-		setTitle("CaMS");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 800, 600);
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);	
+		setLayout(null);	
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(0, 0, 102));
-		panel.setBounds(0, 0, 784, 39);
-		contentPane.add(panel);
+		panel.setBounds(0, 0, 800, 39);
+		add(panel);
 		panel.setLayout(null);
 		
 		JLabel label = new JLabel("#Canteen");
@@ -102,8 +72,8 @@ public class CreateOrderForm extends JFrame {
 		receipt.setBackground(new Color(245, 245, 245));
 		receipt.setBounds(21, 50, 300, 501);
 		JTextArea card = new JTextArea(24, 26);
-		contentPane.add(receipt);
-		contentPane.add(new JScrollPane(card));
+		add(receipt);
+		add(new JScrollPane(card));
 		
 		final JButton btnChickenChop = new JButton();
 		btnChickenChop.setText("Chicken Chop");
@@ -115,7 +85,7 @@ public class CreateOrderForm extends JFrame {
 		btnChickenChop.setBounds(336, 53, 120, 52);
 		btnChickenChop.setBackground(new Color(105, 105, 105));
 		btnChickenChop.setForeground(Color.WHITE);
-		contentPane.add(btnChickenChop);
+		add(btnChickenChop);
 		
 		final JButton btnLambChop = new JButton();
 		btnLambChop.setText("Lamb Chop");
@@ -127,7 +97,7 @@ public class CreateOrderForm extends JFrame {
 		btnLambChop.setBounds(463, 53, 120, 52);
 		btnLambChop.setBackground(new Color(105, 105, 105));
 		btnLambChop.setForeground(Color.WHITE);
-		contentPane.add(btnLambChop);
+		add(btnLambChop);
 		
 		final JButton btnPorkChop = new JButton();
 		btnPorkChop.setText("Pork Chop");
@@ -139,39 +109,39 @@ public class CreateOrderForm extends JFrame {
 		btnPorkChop.setBounds(592, 53, 120, 52);
 		btnPorkChop.setBackground(new Color(105, 105, 105));
 		btnPorkChop.setForeground(Color.WHITE);
-		contentPane.add(btnPorkChop);
+		add(btnPorkChop);
 		
 		JButton btnRibeyeSteak = new JButton("Ribeye Steak");
 		btnRibeyeSteak.setBounds(336, 124, 120, 52);
 		btnRibeyeSteak.setBackground(new Color(105, 105, 105));
 		btnRibeyeSteak.setForeground(Color.WHITE);
-		contentPane.add(btnRibeyeSteak);
+		add(btnRibeyeSteak);
 		
 		JButton btnLambChop_1 = new JButton("Sirloin Steak");
 		btnLambChop_1.setBounds(463, 124, 120, 52);
 		btnLambChop_1.setBackground(new Color(105, 105, 105));
 		btnLambChop_1.setForeground(Color.WHITE);
-		contentPane.add(btnLambChop_1);
+		add(btnLambChop_1);
 		
 		JButton btnBeefSteak = new JButton("Beef Steak");
 		btnBeefSteak.setBounds(592, 124, 120, 52);
 		btnBeefSteak.setBackground(new Color(105, 105, 105));
 		btnBeefSteak.setForeground(Color.WHITE);
-		contentPane.add(btnBeefSteak);
+		add(btnBeefSteak);
 		
 		JButton btnEatIn = new JButton("EAT IN");
 		btnEatIn.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnEatIn.setForeground(Color.WHITE);
 		btnEatIn.setBackground(new Color(0, 128, 0));
 		btnEatIn.setBounds(339, 276, 89, 23);
-		contentPane.add(btnEatIn);
+		add(btnEatIn);
 		
 		JButton btnTakeOut = new JButton("TAKE OUT");
 		btnTakeOut.setForeground(Color.WHITE);
 		btnTakeOut.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnTakeOut.setBackground(new Color(0, 128, 0));
 		btnTakeOut.setBounds(456, 276, 89, 23);
-		contentPane.add(btnTakeOut);
+		add(btnTakeOut);
 			
 		final JButton numPad_1 = new JButton();
 		numPad_1.setText("1");
@@ -185,7 +155,7 @@ public class CreateOrderForm extends JFrame {
 		numPad_1.setBackground(Color.BLACK);
 		//               x  , y  , x-size, y-size
 		numPad_1.setBounds(338, 323, 57, 44);
-		contentPane.add(numPad_1);
+		add(numPad_1);
 		
 		final JButton numPad_2 = new JButton();
 		numPad_2.setText("2");
@@ -198,7 +168,7 @@ public class CreateOrderForm extends JFrame {
 		numPad_2.setForeground(Color.WHITE);
 		numPad_2.setBackground(Color.BLACK);
 		numPad_2.setBounds(408, 323, 57, 44);
-		contentPane.add(numPad_2);
+		add(numPad_2);
 		
 		final JButton numPad_3 = new JButton();
 		numPad_3.setText("3");
@@ -211,7 +181,7 @@ public class CreateOrderForm extends JFrame {
 		numPad_3.setForeground(Color.WHITE);
 		numPad_3.setBackground(Color.BLACK);
 		numPad_3.setBounds(478, 323, 57, 44);
-		contentPane.add(numPad_3);
+		add(numPad_3);
 		
 		final JButton numPad_4 = new JButton();
 		numPad_4.setText("4");
@@ -224,7 +194,7 @@ public class CreateOrderForm extends JFrame {
 		numPad_4.setBackground(Color.BLACK);
 		numPad_4.setFont(new Font("Tahoma", Font.BOLD, 12));
 		numPad_4.setBounds(338, 382, 57, 44);
-		contentPane.add(numPad_4);
+		add(numPad_4);
 		
 		final JButton numPad_5 = new JButton();
 		numPad_5.setText("5");
@@ -237,7 +207,7 @@ public class CreateOrderForm extends JFrame {
 		numPad_5.setBackground(Color.BLACK);
 		numPad_5.setFont(new Font("Tahoma", Font.BOLD, 12));
 		numPad_5.setBounds(408, 382, 57, 44);
-		contentPane.add(numPad_5);
+		add(numPad_5);
 		
 		final JButton numPad_6 = new JButton();
 		numPad_6.setText("6");
@@ -250,7 +220,7 @@ public class CreateOrderForm extends JFrame {
 		numPad_6.setBackground(Color.BLACK);
 		numPad_6.setFont(new Font("Tahoma", Font.BOLD, 12));
 		numPad_6.setBounds(478, 382, 57, 44);
-		contentPane.add(numPad_6);
+		add(numPad_6);
 		
 		final JButton numPad_7 = new JButton();
 		numPad_7.setText("7");
@@ -263,7 +233,7 @@ public class CreateOrderForm extends JFrame {
 		numPad_7.setBackground(Color.BLACK);
 		numPad_7.setFont(new Font("Tahoma", Font.BOLD, 12));
 		numPad_7.setBounds(338, 445, 57, 44);
-		contentPane.add(numPad_7);
+		add(numPad_7);
 		
 		final JButton numPad_8 = new JButton();
 		numPad_8.setText("8");
@@ -276,7 +246,7 @@ public class CreateOrderForm extends JFrame {
 		numPad_8.setBackground(Color.BLACK);
 		numPad_8.setFont(new Font("Tahoma", Font.BOLD, 12));
 		numPad_8.setBounds(408, 445, 57, 44);
-		contentPane.add(numPad_8);
+		add(numPad_8);
 		
 		final JButton numPad_9 = new JButton();
 		numPad_9.setText("9");
@@ -289,25 +259,25 @@ public class CreateOrderForm extends JFrame {
 		numPad_9.setBackground(Color.BLACK);
 		numPad_9.setFont(new Font("Tahoma", Font.BOLD, 12));
 		numPad_9.setBounds(478, 445, 57, 44);
-		contentPane.add(numPad_9);
+		add(numPad_9);
 		
 		JButton btnChickenCutlet = new JButton("Chicken Cutlet");
 		btnChickenCutlet.setBounds(336, 197, 120, 52);
 		btnChickenCutlet.setBackground(new Color(105, 105, 105));
 		btnChickenCutlet.setForeground(Color.WHITE);
-		contentPane.add(btnChickenCutlet);
+		add(btnChickenCutlet);
 		
 		JButton btnPorkCutlet = new JButton("Pork Cutlet");
 		btnPorkCutlet.setBounds(592, 197, 120, 52);
 		btnPorkCutlet.setBackground(new Color(105, 105, 105));
 		btnPorkCutlet.setForeground(Color.WHITE);
-		contentPane.add(btnPorkCutlet);
+		add(btnPorkCutlet);
 		
 		JButton btnFishCutlet = new JButton("Fish Cutlet");
 		btnFishCutlet.setBounds(463, 197, 120, 52);
 		btnFishCutlet.setBackground(new Color(105, 105, 105));
 		btnFishCutlet.setForeground(Color.WHITE);
-		contentPane.add(btnFishCutlet);
+		add(btnFishCutlet);
 		
 		JButton btnX = new JButton("DELETE");
 		btnX.addActionListener(new ActionListener() {
@@ -319,41 +289,41 @@ public class CreateOrderForm extends JFrame {
 		btnX.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnX.setBackground(new Color(0, 0, 0));
 		btnX.setBounds(557, 323, 100, 98);
-		contentPane.add(btnX);
+		add(btnX);
 		
 		JButton btnMainPage = new JButton("Main Page");
 		btnMainPage.setForeground(Color.WHITE);
 		btnMainPage.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnMainPage.setBackground(new Color(255, 0, 0));
 		btnMainPage.setBounds(673, 528, 100, 23);
-		contentPane.add(btnMainPage);
+		add(btnMainPage);
 		
 		JButton btnNextPage = new JButton("Next Page");
 		btnNextPage.setForeground(Color.WHITE);
 		btnNextPage.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnNextPage.setBackground(new Color(100, 149, 237));
 		btnNextPage.setBounds(673, 448, 100, 23);
-		contentPane.add(btnNextPage);
+		add(btnNextPage);
 		
 		JButton btnPrevPage = new JButton("Prev Page");
 		btnPrevPage.setForeground(Color.WHITE);
 		btnPrevPage.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnPrevPage.setBackground(new Color(100, 149, 237));
 		btnPrevPage.setBounds(673, 488, 100, 23);
-		contentPane.add(btnPrevPage);
+		add(btnPrevPage);
 		
 		JButton btnEnter = new JButton("ENTER");
 		btnEnter.setForeground(Color.WHITE);
 		btnEnter.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnEnter.setBackground(Color.BLACK);
 		btnEnter.setBounds(557, 452, 100, 98);
-		contentPane.add(btnEnter);
+		add(btnEnter);
 		
 		JButton button_8 = new JButton("");
 		button_8.setForeground(Color.WHITE);
 		button_8.setFont(new Font("Tahoma", Font.BOLD, 12));
 		button_8.setBackground(Color.BLACK);
 		button_8.setBounds(336, 507, 197, 44);
-		contentPane.add(button_8);
+		add(button_8);
 	}
 }
